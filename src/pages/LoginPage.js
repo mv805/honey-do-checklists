3f3970d6da3620487f9a5e2ed3cb5cdc6e660660
Matt Villa <matthewvilla805@gmail.com>
2022-07-12 13:34:28 -0700

    Updated styling to general page module

--- src/pages/LoginPage.js
+++ src/pages/LoginPage.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import CategoryFrame from '../UI/CategoryFrame';
 import classes from './LoginPage.module.css';
+import pageStyle from './Pages.module.css';
 import { Link } from 'react-router-dom';
 
 const LoginPage = () => {
     return (
-        <div className={ classes.login }>
+        <div className={ pageStyle.page }>
             <CategoryFrame title="Login">
                 <h1>Username</h1>
                 <p>field</p>
