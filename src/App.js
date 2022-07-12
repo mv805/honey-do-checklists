5c8730dad79ac742b7899b5bce7b96cfc94ddad9
Matt Villa <matthewvilla805@gmail.com>
2022-07-12 00:13:41 -0700

    Initial Setup and basic page layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
+import { Switch, Redirect, Route } from 'react-router-dom';
 import './App.css';
+import MainHeader from './components/MainHeader';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <MainHeader />
+      <main>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/login"></Redirect>
+          </Route>
+          <Route path="/login">
+            <LoginPage />
+          </Route>
+        </Switch>
+      </main>
     </div>
   );
 }
---
+++ src/components/MainHeader.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import classes from './MainHeader.module.css';
+
+const MainHeader = () => {
+    return (
+        <nav className={classes['nav-bar']}>
+            nav Bar
+        </nav>
+    );
+};
+
+export default MainHeader;
