94b9bf2f24ce600d6760f8b77b5fee087e36a862
Matt Villa <matthewvilla805@gmail.com>
2022-07-12 13:34:17 -0700

    Added checklist page

--- src/pages/ChecklistPage.js
+++ src/pages/ChecklistPage.js
@@ -1,8 +1,20 @@
 import React from 'react';
+import classes from './ChecklistPage.module.css';
+import pageStyle from './Pages.module.css';
+import CategoryFrame from '../UI/CategoryFrame';
+import uniqid from 'uniqid';
 
-const ChecklistPage = () => {
+const ChecklistPage = (props) => {
     return (
-        <div>ChecklistPage</div>
+        <div className={pageStyle.page}>
+            <CategoryFrame title="My Checklists">
+                <div className={classes.lists}>
+                    {Object.keys(props.data).map(checklist => {
+                        return <button key={uniqid()}>{checklist}</button>
+                    })}
+                </div>
+            </CategoryFrame>
+        </div>
     );
 };
 
