d4d30cc5030a863c3e778a24ae2692861a33d8c1
Matt Villa <matthewvilla805@gmail.com>
2022-07-15 00:31:37 -0700

    Various hover styling tweaks

--- src/UI/CategoryFrame.js
+++ src/UI/CategoryFrame.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import classes from './CategoryFrame.module.css';
+import { faPenToSquare} from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const CategoryFrame = (props) => {
     return (
         <div className={ classes.frame }>
-            <h1>{ props.title }</h1>
+            <div className={classes.header}>
+                <h1>{ props.title }</h1>
+                <button>
+                    <FontAwesomeIcon icon={ faPenToSquare } size="lg" className={classes['header-icon']}/>
+                </button>
+            </div>
             <div className={ classes.window }>
                 { props.children }
             </div>
